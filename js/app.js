0f3699cd52885965d48ac46bd0d1f1adbfcf4013
lekanda <abernaolao@gmail.com>
2020-01-19 12:26:21 +0100

    Cap:198 finalizado. Crear selectores

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let IDBOpenDBRequest;
+// Selectores de la interfaz
+
+const form = document.querySelector('form'),
+      nombreMascota = document.querySelector('#mascota'),
+      nombreCliente = document.querySelector('#cliente'),
+      telefono = document.querySelector('#telefono'),
+      fecha = document.querySelector('#fecha'),
+      hora = document.querySelector('#hora'),
+      sintomas = document.querySelector('#sintomas'),
+      headingAdministra = document.querySelector('#administra');
